8ff25b2c81e807dd95e891d031dbcc98fe0732a9
KPrice725 <kprice725@aol.com>
2017-11-25 23:27:52 -0500

    Update Solution.java

--- Hamming Distance/src/Solution.java
+++ Hamming Distance/src/Solution.java
@@ -1,47 +1,6 @@
 
 public class Solution {
-
-	public static void main(String[] args) {
-
-		
-		
-		System.out.println(Integer.bitCount(5^3));
-		System.out.println(42 ^ 15);
-		System.out.println(hammingDistance(42, 15));
-		
-		System.out.println();
-		System.out.println();
-		
-		
-		int[] values = {0, 536870911, 0, 536870911};
-		System.out.println(totalHammingDistance(values));
-		
-		
-
-	}
-
 	public static int hammingDistance(int x, int y) {
-
 		return Integer.bitCount(x ^ y);
-
 	}
-	
-	public static int totalHammingDistance(int[] nums) {
-		
-		int result = 0;
-		
-		for(int i = 0; i < nums.length; i++) {
-			for(int j = i + 1; j < nums.length; j++) {
-				System.out.println(Integer.bitCount(nums[i] ^ nums[j]));
-				result += Integer.bitCount(nums[i] ^ nums[j]);
-			}
-			
-			
-		}
-		
-		
-		return result;
-	}
-	
-
 }
