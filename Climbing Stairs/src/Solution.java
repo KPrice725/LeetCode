a6a8da304b19031ef98867c5c487ea40f429346b
Kevin Price <kprice725@aol.com>
2019-04-02 09:17:15 -0400

    Add Solution for 70.) Climbing Stairs

---
+++ Climbing Stairs/src/Solution.java
@@ -0,0 +1,42 @@
+/*
+70. Climbing Stairs
+
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Note: Given n will be a positive integer.
+
+Example 1:
+
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+*/
+
+public class Solution {
+	
+	// The solution follows the Fibonacci Sequence!
+	public int climbStairs(int n) {
+		int numWays = 0;
+		int previous = 0;
+		int current = 1;
+		for (int i = 0; i < n; i++) {
+			numWays = previous + current;
+			previous = current;
+			current = numWays;
+		}
+		
+		return numWays;
+	}
+}
